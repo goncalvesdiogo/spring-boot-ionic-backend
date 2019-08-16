00fadb65fa70d470bc368d11a9483136b82cd25b
Diogo R. Gonçalves <goncalvesdiogo@gmail.com>
2019-08-16 10:24:18 -0300

    Estado e Cidade

---
+++ src/main/java/com/goncalvesdiogo/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.goncalvesdiogo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.goncalvesdiogo.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}

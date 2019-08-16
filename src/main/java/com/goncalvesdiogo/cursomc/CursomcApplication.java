83d1d9fd7fee32878882d22bc4e08518cd907b76
Diogo R. Gonçalves <goncalvesdiogo@gmail.com>
2019-08-15 21:10:41 -0300

    Produto e associação de muitos para muitos

--- src/main/java/com/goncalvesdiogo/cursomc/CursomcApplication.java
+++ src/main/java/com/goncalvesdiogo/cursomc/CursomcApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.goncalvesdiogo.cursomc.domain.Categoria;
+import com.goncalvesdiogo.cursomc.domain.Produto;
 import com.goncalvesdiogo.cursomc.repositories.CategoriaRepository;
+import com.goncalvesdiogo.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{
-    
+public class CursomcApplication implements CommandLineRunner {
+
 	@Autowired
 	CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 		System.out.println("Running!!!");
@@ -22,10 +28,22 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-	
-		Categoria cat1 = new Categoria(null,"Informática");
-		Categoria cat2 = new Categoria(null,"Escritório");
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 }

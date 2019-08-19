5b82beea758d3c62c5c92bab6f4fd68a7455f385
Diogo R. Gonçalves <goncalvesdiogo@gmail.com>
2019-08-19 10:05:34 -0300

    Cliente, Tipo Cliente, Telefones e Endereços

--- src/main/java/com/goncalvesdiogo/cursomc/CursomcApplication.java
+++ src/main/java/com/goncalvesdiogo/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.goncalvesdiogo.cursomc.domain.Categoria;
 import com.goncalvesdiogo.cursomc.domain.Cidade;
+import com.goncalvesdiogo.cursomc.domain.Cliente;
+import com.goncalvesdiogo.cursomc.domain.Endereco;
 import com.goncalvesdiogo.cursomc.domain.Estado;
 import com.goncalvesdiogo.cursomc.domain.Produto;
+import com.goncalvesdiogo.cursomc.domain.enums.TipoCliente;
 import com.goncalvesdiogo.cursomc.repositories.CategoriaRepository;
 import com.goncalvesdiogo.cursomc.repositories.CidadeRepository;
+import com.goncalvesdiogo.cursomc.repositories.ClienteRepository;
+import com.goncalvesdiogo.cursomc.repositories.EnderecoRepository;
 import com.goncalvesdiogo.cursomc.repositories.EstadoRepository;
 import com.goncalvesdiogo.cursomc.repositories.ProdutoRepository;
 
@@ -20,16 +25,22 @@ import com.goncalvesdiogo.cursomc.repositories.ProdutoRepository;
 public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
-	CategoriaRepository categoriaRepository;
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	@Autowired
-	ProdutoRepository produtoRepository;
+	private ClienteRepository clienteRepository;
 	
 	@Autowired
-	EstadoRepository estadoRepository;
-	
-	@Autowired
-	CidadeRepository cidadeRepository;
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -45,29 +56,40 @@ public class CursomcApplication implements CommandLineRunner {
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
-		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
-		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-			
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
-		
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
-		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
 		
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
-		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apt 303", "Jardim", "3822084", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
 		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 	}
 }
---
+++ src/main/java/com/goncalvesdiogo/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.goncalvesdiogo.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.goncalvesdiogo.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}

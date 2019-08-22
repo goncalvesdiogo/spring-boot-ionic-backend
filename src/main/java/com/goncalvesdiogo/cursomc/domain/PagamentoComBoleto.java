859f6e575da0ad31a9318289a058927851eea375
Diogo R. Gonçalves <goncalvesdiogo@gmail.com>
2019-08-22 11:54:22 -0300

    Utilizando somente JsonIgnore

--- src/main/java/com/goncalvesdiogo/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/goncalvesdiogo/cursomc/domain/PagamentoComBoleto.java
@@ -3,8 +3,6 @@ package com.goncalvesdiogo.cursomc.domain;
 import java.util.Date;
 
 import javax.persistence.Entity;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.goncalvesdiogo.cursomc.domain.enums.EstadoPagamento;

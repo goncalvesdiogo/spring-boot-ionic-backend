6332bab3dc38a2b8efc7519bb55a4219b4425858
Diogo R. Gonçalves <goncalvesdiogo@gmail.com>
2019-08-24 10:44:52 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/goncalvesdiogo/cursomc/services/CategoriaService.java
+++ src/main/java/com/goncalvesdiogo/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.goncalvesdiogo.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.goncalvesdiogo.cursomc.domain.Categoria;
 import com.goncalvesdiogo.cursomc.repositories.CategoriaRepository;
+import com.goncalvesdiogo.cursomc.services.exceptions.DataIntegrityException;
 import com.goncalvesdiogo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -25,9 +27,18 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma Categoria que possui Produtos associados.");
+		}
 	}
 }
